362344c3bdf7aa33ff2615cc75c1082cd3398e63
Pedro Nevado <pedro@didekin.es>
2018-09-03 19:13:23 +0200

    version didekin-web-local-b.1.2

--- didekin/src/sql_main/db_all_tables.sql
+++ didekin/src/sql_main/db_all_tables.sql
@@ -1,7 +1,7 @@
 -- Usuarios y comunidades
-source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/schema_user_mysql.sql;
-source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/comunidad_autonoma_insert.sql;
-source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/provincia_insert.sql;
+source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/usuario/schema_user_mysql.sql;
+source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/usuario/comunidad_autonoma_insert.sql;
+source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/usuario/provincia_insert.sql;
 -- Incidencias
-source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/schema_incidencia_mysql.sql;
+source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/incidencia/schema_incidencia_mysql.sql;
 
--- didekin/src/sql_main/db_all_views.sql
+++ didekin/src/sql_main/db_all_views.sql
@@ -1,5 +1,5 @@
 -- Usuarios y comunidades
-source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/user_views.sql;
+source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/usuario/user_views.sql;
 -- Incidencias
-source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/incidencia_views.sql;
+source /Users/pedro/Documents/git_projects/git_didekinweb/didekin_web/didekin/src/sql_main/incidencia/incidencia_views.sql;
 
