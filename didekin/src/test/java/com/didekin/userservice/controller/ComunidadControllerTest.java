a9fb636d3be2bb1c24a83bf697c3df4dbb9688da
Pedro Nevado <pedro@didekin.es>
2018-09-11 12:19:12 +0200

    version didekin-web-local-b.1.1

--- didekin/src/test/java/com/didekin/userservice/controller/ComunidadControllerTest.java
+++ didekin/src/test/java/com/didekin/userservice/controller/ComunidadControllerTest.java
@@ -162,8 +162,7 @@ public abstract class ComunidadControllerTest {
     }
 
     @RunWith(SpringJUnit4ClassRunner.class)
-    @SpringBootTest(classes = {Application.class, RetrofitConfigurationDev.class},
-            webEnvironment = DEFINED_PORT)
+    @SpringBootTest(classes = {Application.class, RetrofitConfigurationDev.class})
     @ActiveProfiles(value = {NGINX_JETTY_LOCAL, MAIL_PRE})
     @Category({DbPre.class})
     @DirtiesContext
